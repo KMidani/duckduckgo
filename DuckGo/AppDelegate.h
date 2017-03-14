a9aece14b96201ba74d2cc9b200bbff84a2dd55f
khalil midani <khalil.midani@gmail.com>
2017-03-14 18:06:49 -0400

    Initial Commit

---
+++ DuckGo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DuckGo
+//
+//  Created by khalil midani on 3/14/17.
+//  Copyright © 2017 Khalil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DuckGo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DuckGo
+//
+//  Created by khalil midani on 3/14/17.
+//  Copyright © 2017 Khalil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
